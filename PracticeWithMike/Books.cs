a300ff1a04f0f9b78941d8e454cec35fd76abbbc
Dan Storey <danstorey901@gmail.com>
2022-01-18 21:00:26 -0500

    Add project files.

---
+++ PracticeWithMike/Books.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeWithMike
+{
+
+    class Books
+    {
+        private static int NextId = 1;
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public int Inventory { get; set; }
+        public bool InStock { get; set; }
+
+        public Books()
+        {
+
+        }
+        public Books(string title, string author, int pages, int inventory, bool instock)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Pages = pages;
+            this.Inventory = inventory;
+            this.InStock = instock;
+
+            this.Id = NextId++;
+
+          
+        }
+        public void PrintUserInputData()
+        {
+            Console.WriteLine("Please Input your Title!");
+            this.Title = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Please Input your Author!");
+            this.Author = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Please Input your Number of Pages!");
+            this.Pages = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Input your Inventory Quantity!");
+            this.Inventory = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Input Yes or No for Availability!");
+            this.InStock = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine($"Here are some book recommendations!{this.Id}.|Title = {this.Title}|Author = {this.Author}|Pages = {this.Pages}|Quantity {this.Inventory}|InStock? {this.InStock}");
+        }
+    }
+}
---
+++ PracticeWithMike/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PracticeWithMike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books book1 = new Books("cat in the hat", "jon", 50, 5, true);
+            Books book2 = new Books("his name", "jacob", 2, 1, true);
+            Books book3 = new Books("is my name", "jingle", 2, 1, false);
+            Books book4 = new Books("crime and punishment", "heimer", 500, 100, true);
+            Books book5 = new Books("ordinary men", "schmidt", 350, 56, true);
+
+            Books[] List = { book1, book2, book3, book4, book5};
+            foreach (var Fred in List)
+            {
+                Fred.PrintUserInputData();
+            }
+
+
+        }
+    }
+}
